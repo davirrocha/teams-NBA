091efa6c5beab21c7cc7979752c179a2d9bf8b5d
davirrocha <davirochamoraes7@gmail.com>
2024-05-12 17:41:02 -0300

    alt-codigos

--- document.js
+++ document.js
@@ -1,30 +1,30 @@
-const team = document.querySelector(".times")
-const link = document.querySelector(".link-times")
-const tudo = document.querySelector("body")
-const header = document.querySelector("header")
-const links = document.querySelector(".links")
-const dois = document.querySelector(".dois")
-const tres = document.querySelector(".tres")
-
-function mudarLake() {
-    team.src = "./img/lakerslogo.png"
-    link.href = "./lakers.html"
-    tudo.style.backgroundColor = "#380E73"
-    header.style.backgroundColor = "#F2AC29"
-    links.style.color = "#380E73"
-    dois.style.color = "#380E73"
-    tres.style.color = "#380E73"
+let eats = document.querySelector(".east")
+let west = document.querySelector(".west")
+let eastList = ''
+let westList = ''
 
 
-}
+eastOptions.forEach(item => {
+    eastList += `
+        <li>
+            <a href="${item.link}" target="_blank">
+            <img src="${item.img}">
+             </a>
+            </li>
+        `
+    eats.innerHTML = eastList
 
-function mudarCelt() {
-    team.src = "img/celticslogo.png"
-    link.href = "./celtics.html"
-    tudo.style.backgroundColor = "#047c44"
-    header.style.backgroundColor = "#000000"
-    links.style.color = "#F2AA80"
-    dois.style.color = "#F2AA80"
-    tres.style.color = "#F2AA80"
-    links.hover.color = "#038C4C"
-}
+
+})
+
+
+westOptions.forEach(item => {
+    westList += `
+        <li>
+            <a href="${item.link}" target="_blank">
+            <img src="${item.img}">
+            </a>
+        </li>
+    `
+    west.innerHTML = westList
+})
---
+++ options.js
@@ -0,0 +1,38 @@
+const eastOptions = [
+
+    { img: "./assets/celticslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/bos/boston-celtics" },
+    { img: "./assets/knickslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/ny/new-york-knicks" },
+    { img: "./assets/buckslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/mil/milwaukee-bucks" },
+    { img: "./assets/cavalierslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/cle/cleveland-cavaliers" },
+    { img: "./assets/magiclogo.png", link: "https://www.espn.com.br/nba/time/_/nome/orl/orlando-magic" },
+    { img: "./assets/pacerslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/ind/indiana-pacers" },
+    { img: "./assets/76erslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/phi/philadelphia-76ers" },
+    { img: "./assets/heatlogo.png", link: "https://www.espn.com.br/nba/time/_/nome/mia/miami-heat" },
+    { img: "./assets/bullslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/chi/chicago-bulls" },
+    { img: "./assets/hawkslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/atl/atlanta-hawks" },
+    { img: "./assets/netslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/bkn/brooklyn-nets" },
+    { img: "./assets/raptorslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/tor/toronto-raptors" },
+    { img: "./assets/hornetslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/cha/charlotte-hornets" },
+    { img: "./assets/wizardslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/wsh/washington-wizards" },
+    { img: "./assets/pistonslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/det/detroit-pistons" },
+
+]
+
+const westOptions = [
+    { img: "./assets/thunderlogo.png", link: "https://www.espn.com.br/nba/time/_/nome/okc/oklahoma-city-thunder" },
+    { img: "./assets/nuggetslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/den/denver-nuggets" },
+    { img: "./assets/timberwolveslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/min/minnesota-timberwolves" },
+    { img: "./assets/clipperslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/lac/la-clippers" },
+    { img: "./assets/maverickslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/dal/dallas-mavericks" },
+    { img: "./assets/sunslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/phx/phoenix-suns" },
+    { img: "./assets/lakerslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/lal/los-angeles-lakers" },
+    { img: "./assets/pelicanslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/no/new-orleans-pelicans" },
+    { img: "./assets/kingslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/sac/sacramento-kings" },
+    { img: "./assets/warrioslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/gs/golden-state-warriors" },
+    { img: "./assets/rocketslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/hou/houston-rockets" },
+    { img: "./assets/jazzlogo.png", link: "https://www.espn.com.br/nba/time/_/nome/utah/utah-jazz" },
+    { img: "./assets/grizzlieslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/mem/memphis-grizzlies" },
+    { img: "./assets/spurslogo.png", link: "https://www.espn.com.br/nba/time/_/nome/sa/san-antonio-spurs" },
+    { img: "./assets/traillogo.png", link: "https://www.espn.com.br/nba/time/_/nome/por/portland-trail-blazers" },
+
+]
